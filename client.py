abb9ed3a8dc96dacbe4de024de4e10195090ebde
souvikmaji <souvikmaji94@gmail.com>
2017-03-27 18:00:20 +0530

    cleaned up code, removed references to rfc. Better communication message for server and client

---
+++ client.py
@@ -0,0 +1,197 @@
+import os
+import pickle
+import random
+import socket
+from _thread import *
+
+
+# get file and save
+def p2p_get_request(filename, peer_host, peer_upload_port):
+    s = socket.socket()
+    s.connect((peer_host, int(peer_upload_port)))
+    data = p2p_request_message(filename, host)
+    s.send(bytes(data, 'utf-8'))
+    data_rec = pickle.loads(s.recv(1024 * 10))
+    print("Data_rec", str(data_rec))
+    my_data = data_rec[1]
+    print(my_data)
+    current_path = os.getcwd()
+    filename = filename
+    filename = current_path + "/" + filename
+    with open(filename, 'w') as file:
+        file.write(my_data)
+    with open(filename, 'r') as file:
+        text = file.read()
+        print(text)
+    s.close()
+
+
+# display p2p response message
+def p2p_response_message(filename):
+    current_path = os.getcwd()
+    # filename = current_path + '/' + filename
+    if os.path.exists(filename):
+        status = "200"
+        phrase = "OK"
+        txt = open(filename, "rb")
+        data = txt.read()
+        content_length = os.path.getsize(filename)
+        message = [status + " " + phrase + "\n" + "Content-Length: " + str(content_length) + "\n", str(data)]
+    else:
+        status = "404"
+        phrase = "Not Found"
+        message = status + " " + phrase + "\n"
+    return message
+
+
+# send rfcs to peers
+def send_file(conn, filename):
+    txt = open(filename, "rb")
+    data = txt.read(1024)
+    while data:
+        conn.send(data)
+        data = txt.read(1024)
+    s.close()
+
+
+def recv_file(conn, filename):
+    txt = open(filename, "wb")
+    data = conn.recv(1024)
+    while data:
+        txt.write(data)
+        data = conn.recv(1024)
+    txt.close()
+
+
+# display p2p request message
+def p2p_request_message(file_name, host):
+    message = "GET FILE: " + str(file_name) + " Host: " + str(host) + "\n"
+    return message
+
+
+# display p2s request message for ADD method
+def p2s_add_message(host, port, title):  # for ADD
+    message = "ADD\nHost: " + str(host) + "\n" + "Port: " + str(port) + "\n" \
+                                                                        "Title: " + str(title) + "\n"
+    return [message, host, port, title]
+
+
+# display p2s request message for LOOKUP method
+def p2s_lookup_message(host, port, title, get_or_lookup):  # LOOKUP method
+    message = "LOOKUP\n" + \
+              "Host: " + str(host) + "\n" \
+                                     "Port: " + str(port) + "\n" \
+                                                            "Title: " + str(title) + "\n"
+    return [title, get_or_lookup]
+
+
+# display p2s request message for LIST methods
+def p2s_list_request(host, port):
+    message = "LIST ALL\n" \
+              "Host: " + str(host) + "\n" \
+                                     "Port: " + str(port) + "\n"
+    return message
+
+
+# get the list of the local rfcs
+def get_local_rfcs():
+    rfcs_path = os.getcwd()
+    rfc_files = [num for num in os.listdir(rfcs_path) if os.path.isfile(os.path.join(rfcs_path, num))]
+    return rfc_files
+
+
+# pass peer's hostname, port number and rfc_num, rfc_title
+def peer_information():
+    keys = ["Title"]
+    rfcs_title = get_local_rfcs()  # ["title1", "title2", "title3"] we use rfcs_num to fill in title
+    rfcs_title = []
+    for title in rfcs_title:
+        entry = [title]
+        dict_list_of_files.insert(0, dict(zip(keys, entry)))
+    return [upload_port_num, dict_list_of_files]
+
+
+upload_port_num = 65000 + random.randint(1, 500)  # generate a upload port randomly in 65000~65500
+dict_list_of_files = []  # list of dictionaries of RFC numbers and Titles.
+s = socket.socket()
+host = "127.0.0.1"
+port = 7734
+s.connect((host, port))
+data = pickle.dumps(peer_information())  # send all the peer information to server
+s.send(data)
+data = s.recv(1024)
+print(data.decode('utf-8'))
+
+
+def print_combined_list(dictionary_list, keys):
+    for item in dictionary_list:
+        print(' '.join([item[key] for key in keys]))
+
+
+def get_user_input():
+    # if key press, then close:
+    user_input = input("> Enter ADD, LIST, LOOKUP, GET, or EXIT:  ")
+    if user_input == "EXIT":
+        data = pickle.dumps("EXIT")
+        s.send(data)
+        s.close()  # Close the socket when done
+
+    elif user_input == "ADD":
+        user_input_filename = input("> Enter filename: ")
+        data = pickle.dumps(p2s_add_message(host, upload_port_num, user_input_filename))
+        s.send(data)
+        server_data = s.recv(1024)
+        print(server_data.decode('utf-8'))
+        get_user_input()
+
+    elif user_input == "LIST":
+        data = pickle.dumps(p2s_list_request(host, port))
+        s.send(data)
+        new_data = pickle.loads(s.recv(1024))
+        print_combined_list(new_data[0], new_data[1])
+
+        get_user_input()
+    elif user_input == "GET":
+        user_input_filename = input("> Enter the filename: ")
+        data = pickle.dumps(p2s_lookup_message(host, port, user_input_filename, "0"))
+        s.send(data)
+        server_data = pickle.loads(s.recv(1024))
+        if not server_data[0]:
+            print(server_data[1])
+        else:
+            p2p_get_request(str(user_input_filename), server_data[0]["Hostname"], server_data[0]["Port Number"])
+        get_user_input()
+    elif user_input == "LOOKUP":
+        user_input_filename = input("> Enter filename: ")
+        data = pickle.dumps(p2s_lookup_message(host, port, user_input_filename, "1"))
+        s.send(data)
+        server_data = pickle.loads(s.recv(1024))
+        print(server_data[1], end="")
+        keys = ['Title', 'Hostname', 'Port Number']
+        print_combined_list(server_data[0], keys)
+        get_user_input()
+    else:
+        get_user_input()
+
+
+def p2p_listen_thread(str, i):
+    upload_socket = socket.socket()
+    host = "127.0.0.1"
+    upload_socket.bind((host, upload_port_num))
+    upload_socket.listen(5)
+    while True:
+        c, addr = upload_socket.accept()
+        data_p2p_undecode = c.recv(1024)
+        data_p2p = data_p2p_undecode.decode('utf-8')
+        index1st = data_p2p.index(':')
+        index2nd = data_p2p.index('H')
+        filename = data_p2p[index1st + 2: index2nd - 1]  # get the filename
+        print('Got connection from ', addr, " for file: ", filename)
+        c.send(pickle.dumps(p2p_response_message(filename)))
+
+        c.close()
+
+
+# Start listen thread for file transfer
+start_new_thread(p2p_listen_thread, ("hello", 1))
+get_user_input()
---
+++ server.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python
+
+import pickle
+import socket
+from _thread import *
+
+s = socket.socket()
+host = "127.0.0.1"
+port = 7734
+s.bind((host, port))
+s.listen(5)
+
+peer_list = []  # Global list of dictionaries for peers
+file_list = []  # Global list of dictionaries for files
+combined_list = []
+
+
+def response_message(status):
+    if status == "200":
+        phrase = "OK"
+    elif status == "404":
+        phrase = "Not Found"
+    elif status == "400":
+        phrase = "Bad Request"
+    message = status + " " + phrase + "\n"
+    return message
+
+
+# P2S response message from the server
+def p2s_lookup_response(filename):
+    response = search_combined_dict(filename)
+    if not response:
+        status = "404"
+        phrase = "Not Found"
+        message = status + " " + phrase + "\n"
+        return response, message
+    else:
+        status = "200"
+        phrase = "OK"
+        message = status + " " + phrase + "\n"
+        return response, message
+
+
+def p2s_lookup_response2(filename):
+    response = search_combined_dict2(filename)
+    if len(response) == 0:
+        status = "404"
+        phrase = "Not Found"
+        message = status + " " + phrase + "\n"
+        return response, message
+    else:
+        status = "200"
+        phrase = "OK"
+        message = status + " " + phrase + "\n"
+        return response, message
+
+
+def p2s_add_response(conn, hostname, port, filename):
+    response = "200 OK\n" + filename + " " + str(hostname) + " " + str(port)
+    conn.send(bytes(response, 'utf-8'))
+
+
+def search_combined_dict(filename):
+    for d in combined_list:
+        if d['Title'] == filename:
+            return d
+
+    return False
+
+
+def search_combined_dict2(filename):
+    my_list = []
+    for d in combined_list:
+        if d['Title'] == filename:
+            my_list.append(d)
+
+    return my_list
+
+
+def p2s_list_response(conn):
+    message = response_message("200")
+    conn.send(bytes(message, 'utf-8'))
+
+
+# Takes a list and appends a dictionary of hostname and port number
+def create_peer_list(dictionary_list, hostname, port):
+    keys = ['Hostname', 'Port Number']
+
+    entry = [hostname, str(port)]
+    dictionary_list.insert(0, dict(zip(keys, entry)))
+    return dictionary_list, keys
+
+
+# Creates file_list
+def create_file_list(dictionary_list, dict_list_of_files, hostname):
+    keys = ['Title', 'Hostname']
+
+    for file in dict_list_of_files:
+        filename = file['Title']
+        entry = [filename, hostname]
+        dictionary_list.insert(0, dict(zip(keys, entry)))
+
+    return dictionary_list, keys
+
+
+def create_combined_list(dictionary_list, dict_list_of_files, hostname, upload_port):
+    keys = ['Title', 'Hostname', 'Port Number']
+
+    for file in dict_list_of_files:
+        filename = file['Title']
+        entry = [filename, hostname, str(upload_port)]
+        dictionary_list.insert(0, dict(zip(keys, entry)))
+
+    return dictionary_list, keys
+
+
+# Inserts new Dictionary item to file_list when client makes an ADD request
+def append_to_file_list(dictionary_list, filename, hostname):
+    keys = ['Title', 'Hostname']
+    entry = [filename, hostname]
+
+    dictionary_list.insert(0, dict(zip(keys, entry)))
+    return dictionary_list
+
+
+def append_to_combined_list(dictionary_list, filename, hostname, port):
+    keys = ['Title', 'Hostname', 'Port Number']
+    entry = [filename, hostname, str(port)]
+
+    dictionary_list.insert(0, dict(zip(keys, entry)))
+    return dictionary_list
+
+
+# Prints the list of dictionary items
+def print_dictionary(dictionary_list, keys):
+    for item in dictionary_list:
+        print(' '.join([item[key] for key in keys]))
+
+
+# Deletes the entries associated with the hostname
+def delete_peers_dictionary(dict_list_of_peers, hostname):
+    dict_list_of_peers[:] = [d for d in dict_list_of_peers if d.get('Hostname') != hostname]
+    return dict_list_of_peers
+
+
+# Deletes the entries associated with the hostname
+def delete_rfcs_dictionary(dict_list_of_files, hostname):
+    dict_list_of_files[:] = [d for d in dict_list_of_files if d.get('Hostname') != hostname]
+    return dict_list_of_files
+
+
+def delete_combined_dictionary(combined_dict, hostname):
+    combined_dict[:] = [d for d in combined_dict if d.get('Hostname') != hostname]
+    return combined_dict
+
+
+def return_dict():
+    keys = ['Title', 'Hostname', 'Port Number']
+    return combined_list, keys
+
+
+# Create a thread for each client. This prevents the server from blocking communication with multiple clients
+def client_thread(conn, addr):
+    global peer_list, file_list, combined_list
+    conn.send(bytes('Thank you for connecting', 'utf-8'))
+    print('Got connection from ', addr)
+    data = pickle.loads(conn.recv(1024))  # receive the[upload_port_num,rfc_num, rfcs_title]
+    my_port = data[0]
+    print("upload pot_no: ", my_port)
+    # Generate the peer list and RFC list
+    peer_list, peer_keys = create_peer_list(peer_list, addr[0], data[0])  # change addr[1] to data[0]
+    file_list, file_keys = create_file_list(file_list, data[1], addr[0])
+    combined_list, combined_keys = create_combined_list(combined_list, data[1], addr[0], data[0])
+
+    while True:
+        data = pickle.loads(conn.recv(1024))  # receive the[upload_port_num, rfcs_num, rfcs_title]
+        if data == "EXIT":
+            break
+        if type(data) == str:
+            # LIST
+            # p2s_list_response(conn)
+            new_data = pickle.dumps(return_dict())
+            conn.send(new_data)
+        else:
+            # ADD
+            if data[0][0] == "A":
+                p2s_add_response(conn, data[1], data[2], data[3])  # Put server response message here
+                file_list = append_to_file_list(file_list, data[3], addr[0])
+                combined_list = append_to_combined_list(combined_list, data[3], addr[0], my_port)
+                print_dictionary(file_list, file_keys)
+            if data[1] == "0":
+                # GET
+                new_data = pickle.dumps(p2s_lookup_response(data[0]))
+                conn.send(new_data)
+            elif data[1] == "1":
+                # LOOKUP
+                print(p2s_lookup_response2(data[0]))
+                new_data = pickle.dumps(p2s_lookup_response2(data[0]))
+                conn.send(new_data)
+
+    # Remove the client's info from the dictionaries
+    peer_list = delete_peers_dictionary(peer_list, addr[0])
+    file_list = delete_rfcs_dictionary(file_list, addr[0])
+    combined_list = delete_combined_dictionary(combined_list, addr[0])
+    conn.close()
+
+
+while True:
+    c, addr = s.accept()  # Establish connection with client.
+    start_new_thread(client_thread, (c, addr))
+s.close()
